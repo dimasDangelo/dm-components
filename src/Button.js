20be1ec881506a65731db52ebab395367054c4ac
dimas <diamsdangelo@gmail.com>
2025-01-25 19:53:20 -0300

    teste

---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Button = ({ label, onClick }) => {
+  return (
+    <button onClick={onClick} style={{ padding: '10px 20px', fontSize: '16px' }}>
+      {label}
+    </button>
+  );
+};
