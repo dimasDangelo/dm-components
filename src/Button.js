ebd435acac7d6ec4d840c9fbf745b5b404c73438
dimas <diamsdangelo@gmail.com>
2025-01-25 19:57:16 -0300

    teste propriedade do bottao

--- src/Button.js
+++ src/Button.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Button = ({ label, onClick }) => {
   return (
-    <button onClick={onClick} style={{ padding: '10px 20px', fontSize: '16px' }}>
-      {label}
+    <button onClick={onClick} style={{ padding: '10px 20px', fontSize: '16px', color: "red" }}>
+      {label }
     </button>
   );
 };
